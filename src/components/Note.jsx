2a233a618bd627af6bd087ff9e395a0e461b85b8
seno9jp <seno.kazu@gmail.com>
2022-12-11 14:16:03 +0900

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(probs) {
+    return(
+        <div className="note">
+            <h1>{probs.title}</h1>
+            <p>{probs.content}</p>
+        </div>
+    );
+}
+
+export default Note;
